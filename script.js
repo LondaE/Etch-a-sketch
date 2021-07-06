7a9f86c30406e0c2e6206836d23789baba25444b
Evan Londa <evan.londa@gmail.com>
2021-07-06 16:12:29 -0400

    final version with dynamic grid sizing

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function reset(){
 
+    let newSize = prompt("Enter new grid size", "64");
+
+    if(newSize > 100)
+    {
+        alert("Maximum grid size exceeded");
+        reset();
+    }
     let container = document.querySelector('#container');
     let squares = document.querySelectorAll('.grid-square');
+
     for(let i = 0; i < squares.length; i++)
     {
         container.removeChild(squares.item(i));
     }
-    for(let i = 0; i<256; i++)
+
+    container.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+    container.style.gridTemplateColumns= `repeat(${newSize}, 1fr)`;
+
+    for(let i = 0; i < newSize*newSize; i++)
     {
         let div = document.createElement('div');    
         container.appendChild(div);    
