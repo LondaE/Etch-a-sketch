6ad2cff56c8917035f1ecf0b7ebf2252c9944da5
Evan Londa <evan.londa@gmail.com>
2021-07-06 00:41:39 -0400

    working reset button

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let container = document.querySelector('#container');
-for(let i = 0; i<256; i++)
-{
-    let div = document.createElement('div');    
-    container.appendChild(div);    
-    div.classList.add('grid-square');
-    div.addEventListener('mouseover', () => {div.classList.add('activated')});
+function reset(){
+
+    let container = document.querySelector('#container');
+    let squares = document.querySelectorAll('.grid-square');
+    for(let i = 0; i < squares.length; i++)
+    {
+        container.removeChild(squares.item(i));
+    }
+    for(let i = 0; i<256; i++)
+    {
+        let div = document.createElement('div');    
+        container.appendChild(div);    
+        div.classList.add('grid-square');
+        div.addEventListener('mouseover', () => {div.classList.add('activated')});
+    }
 }
